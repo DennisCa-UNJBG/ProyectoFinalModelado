32686964ead367f71b55b2e65dacd60effac297a
DennisCalderon <dennis.tacna@gmail.com>
2023-01-24 14:48:09 -0500

    Se agrego el algoritmo de Lagrange Se borraron varias cosas que ya venian con la plantilla por defecto Se cambio el CSS del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Proyecto de Modelado Computacional</h1>
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Inicio</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/interpolacion">Interpolación Computacional</RouterLink>
       </nav>
     </div>
   </header>
@@ -24,16 +22,16 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+  margin: 1rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  margin-top: 2rem;
+  text-align: center;
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 24px;
   text-align: center;
   margin-top: 2rem;
 }
@@ -56,30 +54,4 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h2>Integrantes del Proyecto</h2>
+    <ol>
+      <li>Calderon Mamani Dennis Severino</li>
+      <li>Sánchez Illatarqui Franz Patricio</li>
+      <li>Coaquira Quispe Alexis David</li>
+      <li>Patricio Callo Eduardo Yack</li>
+      <li>Mamani Maquera Oscar Eduardo</li>
+      <li>Huanca Chambi José Luis</li>
+    </ol>
   </main>
 </template>
+
+<style>
+h2 {
+  margin-top: 2rem;
+  text-align: center;
+}
+li {
+  margin-left: 2rem;
+}
+</style>
